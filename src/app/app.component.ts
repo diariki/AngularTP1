8e4b30a5f4094c5ab73647da052e18e8ba398262
julien <denis_j001@yahoo.fr>
2019-11-13 16:02:32 +0100

    changement description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,28 +16,27 @@ export class AppComponent {
       }, 2000
     );
   });
-  
-	
-    posts = [
-    {
-		title: 'Machine à laver',
-		content: "Ceci est une Machine à laver mais ca ne va pas du tout parceque ça n'est pas limité telque le souhaite le bonhomme qui a créé ce truc il faut donc changer d'organisation",
-		loveIts: 10,
-		createdAt: new Date('December 17, 1995 03:24:00')
-    },
-    {
-		title: 'Frigo',
-		content: 'Ceci est une Frigo',
-		loveIts: -10,
-		createdAt: new Date('September 11, 1998 18:22:00')
-    },
-    {
-		title: 'Machine à laver',
-		content: 'Ceci est une Machine à laver',
-		loveIts: 0,
-		createdAt: new Date('January 1, 2000 12:01:00')
-    }
-  ];
+  	
+	posts = [
+		{
+			title: 'lave vaisselle',
+			content: "Appareil domestique d'électroménager raccordé à l'eau courante qui sert à laver, rincer et sécher la vaisselle de façon mécanique.",
+			loveIts: 10,
+			createdAt: new Date('December 17, 1995 03:24:00')
+		},
+		{
+			title: 'refrigerateur',
+			content: "Ceci est une Frigo",
+			loveIts: -10,
+			createdAt: new Date('September 11, 1998 18:22:00')
+		},
+		{
+			title: 'Lave-linge',
+			content: "Un lave-linge ou machine à laver le linge, aussi appelé laveuse au Canada francophone et lessiveuse en Belgique, est un appareil électroménager conçu pour nettoyer les vêtements et le linge de maison.",
+			loveIts: 0,
+			createdAt: new Date('January 1, 2000 12:01:00')
+		}
+	];
   
   constructor() {
     setTimeout(
